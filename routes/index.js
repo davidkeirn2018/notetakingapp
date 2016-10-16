a62530511fc8eb2fe75eca9b5c15b715e4c4d148
davidkeirn2018 <davidkeirn2018@u.northwestern.edu>
2016-10-16 12:44:09 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var cntrlHome = require('../controllers/NotesList');
-//var cntrlLogin = require('../controllers/NotesList');
 var cntrlNotesList = require('../controllers/NotesList');
-//var cntrlNotesEdit = require('..controllers/NotesList');
 var cntrlMain = require('../controllers/main');
 
 /* Notes Editor Pages */
-//router.get('/', cntrlNotesList.home);
 router.get('/login', cntrlNotesList.login);
 router.get('/notes', cntrlNotesList.notes);
 router.get('/notes/editor', cntrlNotesList.editNotes);
